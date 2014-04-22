cacea021e10d96c67050cea5478ab9e2962863a6
thomaseger <mail@thomaseger.de>
2014-04-22 13:25:32 +0200

    Extracted commandline interface.

---
+++ shop_cmd.rb
@@ -0,0 +1,58 @@
+require_relative 'shop'
+
+class CommandlineInterface 
+  
+  def initialize
+    banana = Article.new("Banana", 1.5, 10)
+    bread  = Article.new("Bread" , 0.5, 25)
+    juice  = Article.new("Juice" , 1.0, 15)
+
+    @articles = [banana, bread, juice]
+    @shop = Shop.new
+  end
+
+  def run
+    while true 
+      puts "I have "
+      for article in @articles do
+        puts " " + article.stock.to_s + " " + article.name + "s"
+      end
+      
+      name = read("Which article do you want to buy?")
+      amount = read("How many " + name + "s do you want to buy?")
+      article = findArticle(name)
+
+      if @shop.sell(amount.to_i, article)
+        puts "Ok, thanks. Next please!"
+      else
+        puts "Sorry not enough in stock."
+      end
+      puts 
+    end
+  end
+
+private 
+  def read(msg)
+    puts msg
+    print "> "
+    input = gets.chomp
+
+    if input.eql? "exit"
+      exit
+    else
+      input
+    end
+  end
+
+  def findArticle(name) 
+    for article in @articles
+      if name.eql? article.name
+        return article
+      end
+    end
+  end
+
+end
+
+commandlineInterface = CommandlineInterface.new
+commandlineInterface.run
